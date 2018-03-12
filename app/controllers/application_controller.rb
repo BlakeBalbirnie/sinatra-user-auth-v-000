4ad83f04bd259954ad79e1d5c118389eced01a66
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-03-12 18:17:58 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do 
+  get '/' do
     erb :home
   end
 
@@ -34,15 +34,15 @@ class ApplicationController < Sinatra::Base
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
+  get '/sessions/logout' do
     session.clear
-    
+
     redirect '/'
   end
 
   get '/users/home' do
     @user = User.find(session[:id])
-   
+
     erb :'/users/home'
   end
 
